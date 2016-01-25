797e27ae8b588f97a14fd1ff39e89d4a7d99fe19
Vedran Arnautovic <vedran.arnautovic@gmail.com>
2016-01-25 14:57:17 +0100

    cleaning up the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,39 +5,24 @@ var browserSync = require('browser-sync');
 
 
 //compile less to css
-
 gulp.task('less', function(){
   return gulp.src('src/less/**/*.less')		//grab all less files
-    .pipe(less()) 							//run the less task on them
-    .pipe(gulp.dest('src/css'))				//and put the output in the css folder
-    .pipe(browserSync.reload({				//let gulp inject css into the browser
+    .pipe(less()) 							          //run the less task on them
+    .pipe(gulp.dest('src/css'))				    //and put the output in the css folder
+    .pipe(browserSync.reload({				    //let gulp inject css into the browser
       stream: true
     }))
 })
 
 
-
 //watch files for changes
-
 gulp.task('serve', ['less', 'browserSync'], function(){
-
-  // browserSync.init({
-  //   server: "src"
-  // });
-
-
-
-  //getting browsersync to work
   gulp.watch('src/less/**/*.less', ['less']); 
   gulp.watch('src/*.html', browserSync.reload); 
-  // gulp.watch("src/*.html").on('change', browserSync.reload); //alternative
-  // gulp.watch('src/js/**/*.js', browserSync.reload);
-  // add other watchers here
 })
 
 
 //update the browser
-
 gulp.task('browserSync', function() {
   browserSync({
     server: {
