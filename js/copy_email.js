e1b560a0a5736faf10050fbe7ac5680622a963fe
Vedran Arnautovic <vedran.arnautovic@gmail.com>
2017-04-08 23:36:30 +1000

    finalised copy email button

--- js/copy_email.js
+++ js/copy_email.js
@@ -1,4 +1,4 @@
-function copyEmailToClipBoard(targetClass, elementID, originalLinkClass, originalLinkContent)
+function copyEmailToClipBoard(targetClass, elementID, originalLabelClass, originalLabelContent)
 {
   // copy to clipboard
   var clipboard = new Clipboard(targetClass);
@@ -6,23 +6,25 @@ function copyEmailToClipBoard(targetClass, elementID, originalLinkClass, origina
   // if the copy succeeded
   clipboard.on('success', function(event) {
     // temporarily change the content and appearance of the thing the user clicked on
-    var email = document.getElementById(elementID);
-    // email.className = "email-coppied-confirmation";
+    var emailButton = document.getElementById(elementID);
     event.clearSelection();
     event.trigger.textContent = 'email copied';
     // then, after a timeout, set it back to it's original content and appearance
     window.setTimeout(function() {
-        email.className = originalLinkClass;
-        event.trigger.textContent = originalLinkContent;
+        emailButton.className = originalLabelClass;
+        event.trigger.textContent = originalLabelContent;
     }, 2000);
   });
 }
 
-function mouseOver(targetElement)
-{
+function mouseOver(targetElement){
     document.getElementById(targetElement).innerHTML = "copy email";
 }
 
-function mouseOut(targetElement, originalLinkContent) {
-    document.getElementById(targetElement).innerHTML = originalLinkContent;
+function mouseOut(targetElement, originalLabelContent) {
+    document.getElementById(targetElement).innerHTML = originalLabelContent;
+}
+
+function touchEnd(targetElement, originalButtonClass){
+    document.getElementById(targetElement).removeClass('hover');
 }
